10e82d3def53d0cc9d2929145212dc2d4ef8a455
Bogdan Rybchynskiy <shekelua@gmail.com>
2018-11-03 02:09:05 +0200

    add dev dependencies

--- config/app.php
+++ config/app.php
@@ -163,7 +163,10 @@ return [
         /*
          * Package Service Providers...
          */
-
+        \hisorange\BrowserDetect\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -171,6 +174,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
@@ -221,7 +225,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Browser' => \hisorange\BrowserDetect\Facade::class,
     ],
 
 ];
